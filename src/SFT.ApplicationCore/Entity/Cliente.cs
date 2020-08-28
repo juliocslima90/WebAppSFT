f7133aac11061589ce4a8ad9b0ff9420b4a007e5
gessicaaragao <gssica.aragao@gmail.com>
2020-08-28 20:32:17 -0300

    Add project files.

---
+++ src/SFT.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFT.ApplicationCore.Entity
+{
+    class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}
